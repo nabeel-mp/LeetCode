458e035e52889ef27069cbc009dae817b725e333
Nabeelmp <mr.nabeelmp@gmail.com>
2025-12-24 10:08:04 +0530

    Time: 5 ms (67.11%), Space: 5.6 MB (87.21%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.go
@@ -0,0 +1,26 @@
+func numIslands(grid [][]byte) int {
+	rows, cols := len(grid), len(grid[0])
+	count := 0
+
+	var dfs func(int, int)
+	dfs = func(r, c int) {
+		if r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0' {
+			return
+		}
+		grid[r][c] = '0'
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == '1' {
+				count++
+				dfs(i, j)
+			}
+		}
+	}
+	return count
+}
