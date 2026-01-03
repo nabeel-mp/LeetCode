a31ee727504c1de757767c52ba926ebbcbcecc1b
Nabeelmp <mr.nabeelmp@gmail.com>
2026-01-03 17:41:00 +0530

    Time: 1 ms (53.07%), Space: 5.1 MB (47.37%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.go
+++ 0012-integer-to-roman/0012-integer-to-roman.go
@@ -1,12 +1,12 @@
 func intToRoman(num int) string {
     values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-    symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+    char := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
     var result string
 
     for i :=0; i< len(values); i++ {
         for num >= values[i] {
-            result +=symbols[i]
+            result +=char[i]
             num -= values[i]
         }
     }
